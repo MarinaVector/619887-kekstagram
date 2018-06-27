2cce5042034db478977c503852a6b33599c3bf3a
Keks <keks@htmlacademy.ru>
2018-06-27 10:04:05 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/pictures.js
+++ 2/js/pictures.js
@@ -7,11 +7,6 @@ var MIN_LIKES = 15;
 var MAX_LIKES = 200;
 var USERS_PHOTOS_COUNT = 25;
 
-var pictureTemplate = document.querySelector('#picture').content;
-var picturesRenderBlock = document.querySelector('.pictures');
-var bigPicture = document.querySelector('.big-picture');
-var commentsList = bigPicture.querySelector('.social__comments');
-
 var COMMENTS_STORAGE = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -58,16 +53,22 @@ var generateRandomValue = function (array) {
   return array[Math.floor(Math.random() * array.length)];
 };
 
-var generateRandomValueList = function (array) {
-  var randomValueList = [];
+var generateRandomIndexArray = function (array) {
+  var randomIndex = Math.floor(Math.random() * array.length);
+  return randomIndex;
+};
 
-  randomValueList.length = Math.floor(Math.random() * array.length);
 
-  for (var i = 0; i < randomValueList.length; i++) {
-    randomValueList[i] = generateRandomValue(array);
+var generateComment = function (array) {
+  var randomUniqValueArrayOne = array[generateRandomIndexArray(array)];
+  var randomUniqValueArrayTwo = array[generateRandomIndexArray(array)];
+  var randomUniqValueArray = randomUniqValueArrayOne + ' ' + randomUniqValueArrayTwo;
+
+  if (randomUniqValueArrayOne === randomUniqValueArrayTwo) {
+    randomUniqValueArray = randomUniqValueArrayTwo;
   }
 
-  return randomValueList;
+  return randomUniqValueArray;
 };
 
 var generateUsersPhotosObjects = function (count) {
@@ -80,7 +81,7 @@ var generateUsersPhotosObjects = function (count) {
     var userPhotoObject = {
       url: 'photos/' + uniqArray[i] + '.jpg',
       likes: generateNumber(MIN_LIKES, MAX_LIKES),
-      comments: generateRandomValueList(COMMENTS_STORAGE),
+      comments: generateComment(COMMENTS_STORAGE),
       description: generateRandomValue(DESCRIPTION_STORAGE)
     };
 
@@ -90,46 +91,105 @@ var generateUsersPhotosObjects = function (count) {
   return usersPhotos;
 };
 
-var usersPhotoSet = generateUsersPhotosObjects(USERS_PHOTOS_COUNT);
-
 var createPicture = function (userPhoto) {
   var pictureElement = pictureTemplate.cloneNode(true);
 
   pictureElement.querySelector('.picture__img').src = userPhoto.url;
   pictureElement.querySelector('.picture__stat--likes').textContent = userPhoto.likes;
-  pictureElement.querySelector('.picture__stat--comments').textContent = userPhoto.comments.length;
+  pictureElement.querySelector('.picture__stat--comments').textContent = generateRandomIndexArray(getUniqArray(MIN_URL, MAX_URL_AVATAR));
 
   return pictureElement;
 };
 
-var getFragment = function (functionName, array) {
+var getFragment = function (array) {
   var fragment = document.createDocumentFragment();
 
   for (var i = 0; i < array.length; i++) {
-    fragment.appendChild(functionName(array[i]));
+    fragment.appendChild(createPicture(array[i]));
   }
 
   return fragment;
 };
 
-picturesRenderBlock.appendChild(getFragment(createPicture, usersPhotoSet));
+var renderPictures = function (arrayObjects) {
+  var picturesRenderBlock = document.querySelector('.pictures');
 
-document.querySelector('.big-picture').classList.remove('hidden');
+  picturesRenderBlock.appendChild(getFragment(arrayObjects));
+};
 
-bigPicture.querySelector('.big-picture__img img').src = usersPhotoSet[0].url;
-bigPicture.querySelector('.likes-count').textContent = usersPhotoSet[0].likes;
-bigPicture.querySelector('.comments-count').textContent = usersPhotoSet[0].comments.length;
-bigPicture.querySelector('.social__caption').textContent = usersPhotoSet[0].description;
+var generateCommentsObjects = function (count) {
+  var usersComments = [];
 
-var avatarCommentUrlNumber = getUniqArray(MIN_URL, MAX_URL_AVATAR);
+  var uniqArray = getUniqArray(MIN_URL, MAX_URL_AVATAR);
 
-var commentsArray = commentsList.children;
+  if (count === 0) {
+    count++;
+  }
 
-for (var i = 0; i < commentsArray.length; i++) {
-  commentsArray[i].classList.add('social__comment--text');
-  commentsArray[i].querySelector('img').src = 'img/avatar-' + avatarCommentUrlNumber[i] + '.svg';
-  commentsArray[i].querySelector('.social__text').textContent = usersPhotoSet[i].comments;
-}
+  for (var i = 0; i < count; i++) {
+
+    var userCommentObject = {
+      url: 'img/avatar-' + uniqArray[i] + '.svg',
+      commentText: generateComment(COMMENTS_STORAGE),
+    };
+
+    usersComments[i] = userCommentObject;
+  }
+
+  return usersComments;
+};
+
+var createCommentElement = function (arrayObject) {
+
+  var commentElement = commentTemplate.querySelector('li').cloneNode(true);
+
+  commentElement.classList.add('social__comment--text');
+  commentElement.querySelector('img').src = arrayObject.url;
+  commentElement.querySelector('.social__text').textContent = arrayObject.commentText;
+
+  return commentElement;
+};
+
+var renderCommentsList = function (array) {
+  var fragment = document.createDocumentFragment();
+
+  for (var i = 0; i < array.length; i++) {
+    fragment.appendChild(createCommentElement(array[i]));
+  }
+
+  return fragment;
+};
+
+var renderBigPicture = function (arrayObjects) {
+  document.querySelector('.big-picture').classList.remove('hidden');
+
+  var usersCommentsSet = generateCommentsObjects(generateRandomIndexArray(getUniqArray(MIN_URL, MAX_URL_AVATAR)));
+
+  bigPicture.querySelector('.big-picture__img img').src = arrayObjects[0].url;
+  bigPicture.querySelector('.likes-count').textContent = arrayObjects[0].likes;
+  bigPicture.querySelector('.comments-count').textContent = usersCommentsSet.length;
+  bigPicture.querySelector('.social__caption').textContent = arrayObjects[0].description;
+
+  commentsList.appendChild(renderCommentsList(usersCommentsSet));
+};
+
+var commentDelete = function () {
+  var commentArray = commentsList.querySelectorAll('li');
+
+  for (var i = 0; i < commentArray.length; i++) {
+    commentArray[i].remove();
+  }
+
+};
+
+var commentTemplate = document.querySelector('#someCommentTemplate').content;
+var pictureTemplate = document.querySelector('#picture').content;
+var bigPicture = document.querySelector('.big-picture');
+var commentsList = bigPicture.querySelector('.social__comments');
+var usersPhotoSet = generateUsersPhotosObjects(USERS_PHOTOS_COUNT);
+commentDelete();
+renderPictures(usersPhotoSet);
+renderBigPicture(usersPhotoSet);
 
 bigPicture.querySelector('.social__comment-count').classList.add('visually-hidden');
 
